ac795ee28f2cfb43c5223ee2991ed481e4f5910c
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-03-23 19:24:30 +0900

    default template engine is haml & stylesheets is sass

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
+    config.generators do |g|
+      g.template_engine :haml
+    end
+
+    config.sass.preferred_syntax = :sass    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
