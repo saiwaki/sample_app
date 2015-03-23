ee8c256c30d443c35868cb185b4584fabf21fa59
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-03-24 00:13:53 +0900

    generate RegistraionsController & SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'registrations/new'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
