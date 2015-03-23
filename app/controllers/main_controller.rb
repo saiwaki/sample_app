726d5e3a594cce9305f7ceaa438ee6b63e75367c
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-03-23 20:20:45 +0900

    view作成

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,23 @@
 class MainController < ApplicationController
   def index
   end
+
+  def show
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    logger.debug params
+    render plain: params[:main].inspect
+  end
+
+  private
+
+  def main_params
+    params.require(:main).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'main/index'
+  get 'main/index', to: "main#index"
+  get 'main/show', to: "main#show"
+  get 'main/new', to: "main#new"
+  post 'main/create', to: "main#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
