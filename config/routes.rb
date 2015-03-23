a90182c4a8f19f12a968715d0d01176730840884
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-03-24 00:15:38 +0900

    edit router

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
-  get 'registrations/new'
+  resource :registraions, only: [:new, :create]
+  resource :sessions, only: [:new, :create, :destroy]
 
-  resources :users
-
+  root to: "registraions#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
