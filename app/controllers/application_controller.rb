fb1533431c874bed91c8d078eedeed21ff55e21a
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-04-17 11:05:59 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  insta = Instagram.user_search("toritori0123")
-  images = Instagram.user_recent_media(insta[0][:id], {count:5})
+  # insta = Instagram.user_search("toritori0123")
+  # images = Instagram.user_recent_media(insta[0][:id], {count:5})
 
-  images.each do |img|
-    img_url = img[:images][:standard_resolution][:url]
-    filename = File.basename(img_url)
+  # images.each do |img|
+  #   img_url = img[:images][:standard_resolution][:url]
+  #   filename = File.basename(img_url)
 
-    open("public/#{filename}", 'wb') do |output|
-      open(img_url) do |data|
-        output.write(data.read)
-      end
-    end
-  end
+  #   open("public/#{filename}", 'wb') do |output|
+  #     open(img_url) do |data|
+  #       output.write(data.read)
+  #     end
+  #   end
+  # end
 
 end
